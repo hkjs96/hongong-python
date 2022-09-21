1cb9cf8de2bddef5e5511ef7aef576fe142c8cd6
hkjs96 <hkjs96@naver.com>
2022-09-21 17:47:26 +0900

    save

--- chap05/fibonacci_recursion02.py
+++ chap05/fibonacci_recursion02.py
@@ -4,7 +4,7 @@ counter = 0
 # 함수를 선언합니다.
 def fibonacci(n):
     # 어떤 피보나치 수를 구하는지 출력합니다.
-    print("fibonacci({})를 구합니다.".format(n))
+    # print("fibonacci({})를 구합니다.".format(n))
     global counter
     counter += 1
     # 피보나치 수를 구합니다.
